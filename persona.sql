345521ec63141d019e602f57b1452ed1c9797545
juanjosebernal <43830885+juanjosebernal@users.noreply.github.com>
2018-11-27 22:07:20 -0500

    Add files via upload

---
+++ persona.sql
@@ -0,0 +1,14 @@
+
+
+
+-- Tabla Persona
+
+create table persona(
+   idpersona int not null,
+   nombres_persona varchar(20),
+   apellidos_persona varchar(20),
+   email_persona varchar(20),
+   usuario_persona varchar(12),
+   contraseña_persona varchar(10)
+   );
+   
---
+++ usuario.sql
@@ -0,0 +1,12 @@
+
+
+
+-- Tabla Usuario
+
+create table usuario(
+   idpersona int not null,
+   email_usuario varchar(20),
+   email_usuario varchar(20)
+   );
+   
+   
